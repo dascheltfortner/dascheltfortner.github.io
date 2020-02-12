ce87e15d14b1f943feeace456d8f62df50ae2243
= <=>
2020-02-12 17:21:33 -0500

    Fixed naming issues

--- header.js
+++ header.js
@@ -1,20 +1,20 @@
 /*
  *  Lookup tables: use these arrays to index the day and month from a Date.
  * */
-var months = [ 'January', 'February', 'March', 'April', 'May', 'June',
+let months = [ 'January', 'February', 'March', 'April', 'May', 'June',
                'July', 'August', 'September', 'October', 'November',
                'December' ];
-var days   = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+let days   = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
                'Friday', 'Saturday' ];
 
 // Get the root element
-var root = document.getElementById('header');
+let root = document.getElementById('header');
 
 // Create the image container
-var bannerContainer = document.createElement('div');
+let bannerContainer = document.createElement('div');
 
 // Create the image banner
-var banner        = document.createElement('img');
+let banner        = document.createElement('img');
 banner.setAttribute('src', 'images/old-timey-banner.svg');
 banner.setAttribute('alt', 'You can\'t handle a thousand words');
 
@@ -22,13 +22,13 @@ banner.setAttribute('alt', 'You can\'t handle a thousand words');
 bannerContainer.appendChild(banner);
 
 // Create the date bar
-var dateBar    = document.createElement('div');
+let dateBar    = document.createElement('div');
 dateBar.setAttribute('id', 'date-bar');
-var dateHeader = document.createElement('h4');
+let dateHeader = document.createElement('h4');
 
 // Add the date to the header
-var today   = new Date(Date.now());
-var dateStr = days[today.getDay()] + ', ' + months[today.getMonth()] + ' ' +
+let today   = new Date(Date.now());
+let dateStr = days[today.getDay()] + ', ' + months[today.getMonth()] + ' ' +
                 today.getDate() + ', ' + today.getFullYear();
 dateHeader.innerHTML = dateStr;
 
