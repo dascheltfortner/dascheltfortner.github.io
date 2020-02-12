d4e48850c3d694ef5d6177ff8d51d40dee4f0e36
= <=>
2020-02-12 17:57:29 -0500

    Updated the post manager

--- posts/post-manager.js
+++ posts/post-manager.js
@@ -4,8 +4,9 @@ const POSTS = {
 
 const setPostContent = post => {
   fetch(`posts/${post}`).then(res => {
-    let postContainer = document.getElementById('post-content')
-    console.log(res)
-    postContainer.innerHTML = res.text()
+    res.text().then(text => {
+      let postContainer = document.getElementById('post-content')
+      document.getElementById('post-content').innerHTML = text
+    })
   })
 }
