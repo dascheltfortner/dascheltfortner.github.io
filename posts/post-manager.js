e1fdb340a4f86a68b300f6ce109a4b2156c8c823
= <=>
2020-02-12 20:14:46 -0500

    Moved over to blog design

--- posts/post-manager.js
+++ posts/post-manager.js
@@ -1,12 +1,27 @@
-const POSTS = {
-  'On Righteousness': 'on-righteousness.txt'
-}
+const POSTS = [
+  { title: 'On Righteousness', path: 'on-righteousness.txt' }
+]
 
 const setPostContent = post => {
   fetch(`posts/${post}`).then(res => {
     res.text().then(text => {
       let postContainer = document.getElementById('post-content')
       document.getElementById('post-content').innerHTML = text
+    })
+  })
+}
+
+const getPost = index => {
+  let post = POSTS[index]
+  return fetch(`./posts/${post.path}`, { mode: 'no-cors' })
+}
+
+const setPostPreview = () => {
+  getPost(0).then(post => {
+    post.text().then(text => {
+      let previewText = `${text.substring(0, 500)}...`
+      let previewContainer = document.getElementById('post-content')
+      previewContainer.innerHTML = previewText
     })
   })
 }
