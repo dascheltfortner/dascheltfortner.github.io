51d1271b83e388c5087f1fa6c642a187c46ed518
= <=>
2020-02-12 20:45:34 -0500

    Updated the new posts pipeline

--- posts/post-manager.js
+++ posts/post-manager.js
@@ -2,31 +2,30 @@ const POSTS = [
   { title: 'On Righteousness', path: 'on-righteousness.txt' }
 ]
 
-const setPostContent = post => {
-  fetch(`posts/${post}`).then(res => {
-    res.text().then(text => {
-      let postContainer = document.getElementById('post-content')
-      document.getElementById('post-content').innerHTML = text
-    })
-  })
-}
-
 const getPost = index => {
   let post = POSTS[index]
-  return fetch(`./posts/${post.path}`, { mode: 'no-cors' })
+  return fetch(`./posts/${post.path}`)
 }
 
 const setPostPreview = () => {
-  // Update the post's title
   let postTitleContainer = document.getElementById('new-post-title')
   postTitleContainer.innerHTML = POSTS[0].title
 
   getPost(0).then(post => {
     post.text().then(text => {
-      // Update the preview text
       let previewText = `${text.substring(0, 1000)}...`
       let previewContainer = document.getElementById('post-content')
       previewContainer.innerHTML = previewText
+    })
+  })
+}
+
+const viewPost = index => {
+  document.getElementById('post-title').innerHTML = POSTS[index].title
+
+  getPost(index).then(post => {
+    post.text().then(text => {
+      document.getElementById('post-content').innerHTML = text
     })
   })
 }
